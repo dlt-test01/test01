f185ede1d9c8e95c94578cd20aec4170e477abfd
dlt-gengshu <dlt_gengshu@163.com>
2016-07-30 10:17:52 +0800

    Update Form1.cs

--- test01/Form1.cs
+++ test01/Form1.cs
@@ -1,25 +1,21 @@
 #region Using
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
-
 using Gizmox.WebGUI.Common;
 using Gizmox.WebGUI.Forms;
-
 #endregion
-
 namespace test01
 {
     public partial class Form1 : Form
     {
+        private string test;
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void button1_Click(object sender, EventArgs e)
